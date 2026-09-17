9f114780e3c48fc9968fdf5ede324b9ad93710d5
Sanket Ramchandra Nirmalkar <165939004+nirmalkars@users.noreply.github.com>
2026-09-17 07:22:25 +0530

    Update streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -5,15 +5,15 @@ import requests
 import streamlit as st
 
 
-# API_URL = "http://127.0.0.1:8000"
+API_URL = "http://127.0.0.1:8000"
 import os
 
-API_URL = st.secrets.get(
-    "API_URL",
-    os.getenv("API_URL", "http://127.0.0.1:8000")
-).rstrip("/")
+# API_URL = st.secrets.get(
+#     "API_URL",
+#     os.getenv("API_URL", "http://127.0.0.1:8000")
+# ).rstrip("/")
 
-st.write("Backend URL:", API_URL)
+# st.write("Backend URL:", API_URL)
 
 st.set_page_config(
     page_title="Policy-Aware Claim Decision Engine",
